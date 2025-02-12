a5461d52391b82482912a6628ace142da60fb005
Chandler <r0l069h@gmail.com>
2025-02-12 18:02:52 +0530

    Add a Login Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import LoginPage from "./LoginPage";
 
 const App = () => {
   const [users, setUsers] = useState([]);
@@ -10,14 +11,7 @@ const App = () => {
 
   useEffect(() => {
     // Fetch users on component mount
-    axios
-      .get("http://localhost:3333/api/v1/users")
-      .then((response) => {
-        setUsers(response.data);
-      })
-      .catch((error) => {
-        console.error("There was an error fetching the users!", error);
-      });
+    
   }, []);
 
   // Handle creating a new user
@@ -47,54 +41,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>User Management</h1>
-
-      <div>
-        <h2>Create User</h2>
-        <input
-          type="text"
-          placeholder="Name"
-          value={newUser.name}
-          onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={newUser.email}
-          onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
-        />
-        <button onClick={handleCreateUser}>Add User</button>
-      </div>
-
-      <div>
-        <h2>Search User by Name</h2>
-        <input
-          type="name"
-          placeholder="Name"
-          value={searchName}
-          onChange={(e) => setSearchName(e.target.value)}
-        />
-        <button onClick={handleSearchUser}>Search</button>
-
-        {searchResult && (
-          <div>
-            <h3>Search Result</h3>
-            <p>Name: {searchResult.name}</p>
-            <p>Email: {searchResult.email}</p>
-          </div>
-        )}
-      </div>
-
-      <div>
-        <h2>Users List</h2>
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>
-              {user.name} - {user.email}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <LoginPage />
     </div>
   );
 };
